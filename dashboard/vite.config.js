e1126e3cecefb7fd36d473df2e30b417c7a6b4af
karrelday <hed-kedayag@smu.edu.ph>
2025-07-21 10:29:22 +0800

    chore: add Vercel configuration for URL rewrites

--- dashboard/vite.config.js
+++ dashboard/vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
+  server: {
     host: '0.0.0.0'
+  },
+  build: {
+    outDir: 'dist'
   }
 })
